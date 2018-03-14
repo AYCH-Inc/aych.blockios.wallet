f9e252c2c8d5cfada6808cdc23e55027d9d5dec9
woudini <10579422+woudini@users.noreply.github.com>
2018-03-14 17:13:03 -0400

    refactor(Settings): remove btc subunits

--- Blockchain/MultiAddressResponse.m
+++ Blockchain/MultiAddressResponse.m
@@ -45,14 +45,12 @@
 +(CurrencySymbol*)btcSymbolFromCode:(NSString *)code
 {
     NSDictionary *btc = @{DICTIONARY_KEY_SYMBOL: CURRENCY_SYMBOL_BTC, DICTIONARY_KEY_CONVERSION: CURRENCY_CONVERSION_BTC, DICTIONARY_KEY_NAME: CURRENCY_NAME_BTC};
-    NSDictionary *mBtc = @{DICTIONARY_KEY_SYMBOL: CURRENCY_SYMBOL_MBC, DICTIONARY_KEY_CONVERSION: CURRENCY_CONVERSION_MBC, DICTIONARY_KEY_NAME: CURRENCY_NAME_MBC};
-    NSDictionary *bits = @{DICTIONARY_KEY_SYMBOL: CURRENCY_SYMBOL_UBC, DICTIONARY_KEY_CONVERSION: CURRENCY_CONVERSION_UBC, DICTIONARY_KEY_NAME: CURRENCY_NAME_UBC};
     
-    NSDictionary *btcCurrencies = @{CURRENCY_CODE_BTC: btc, CURRENCY_CODE_MBC: mBtc, CURRENCY_CODE_UBC: bits};
+    NSDictionary *btcCurrencies = @{CURRENCY_CODE_BTC: btc};
     
     CurrencySymbol * symbol = [[CurrencySymbol alloc] init];
-    symbol.code = code;
-    NSDictionary * currency = [btcCurrencies objectForKey:code];
+    symbol.code = CURRENCY_CODE_BTC;
+    NSDictionary * currency = [btcCurrencies objectForKey:CURRENCY_CODE_BTC];
     symbol.symbol = [currency objectForKey:DICTIONARY_KEY_SYMBOL];
     symbol.conversion = [[currency objectForKey:DICTIONARY_KEY_CONVERSION] longLongValue];
     symbol.name = [currency objectForKey:DICTIONARY_KEY_NAME];

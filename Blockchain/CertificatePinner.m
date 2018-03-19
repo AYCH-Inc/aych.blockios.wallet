9d8c08fc4ce1918957415e60b23794d7dd097c7d
Maurice Achtenhagen <maurice@blockchain.com>
2018-03-19 10:26:29 -0400

    refactor(Config): use computed properties instead of class functions

--- Blockchain/CertificatePinner.m
+++ Blockchain/CertificatePinner.m
@@ -15,7 +15,7 @@
 - (void)pinCertificate
 {
     NSURLSession *session = [SessionManager sharedSession];
-    NSURL *url = [NSURL URLWithString:[NSBundle urlForWallet]];
+    NSURL *url = [NSURL URLWithString:[NSBundle walletUrl]];
     session.sessionDescription = url.host;
     NSURLSessionDataTask *task = [session dataTaskWithURL:url completionHandler:^(NSData * _Nullable data, NSURLResponse * _Nullable response, NSError * _Nullable error) {
         // response management code

b5372db1c2b41e6bf169b74a57b003e35664ff99
woudini <10579422+woudini@users.noreply.github.com>
2018-03-27 14:33:13 -0400

    feat(Dashboard): extend UIScrollView height

--- Blockchain/BCPriceChartContainerViewController.m
+++ Blockchain/BCPriceChartContainerViewController.m
@@ -7,16 +7,21 @@
 //
 
 #import "BCPriceChartContainerViewController.h"
+#import "UIView+ChangeFrameAttribute.h"
 
 #define DICTIONARY_KEY_BITCOIN @"bitcoin"
 #define DICTIONARY_KEY_ETHER @"ether"
 #define DICTIONARY_KEY_BITCOIN_CASH @"bitcoinCash"
 
 @class ChartAxisBase;
-@interface BCPriceChartContainerViewController ()
+@interface BCPriceChartContainerViewController () <UIScrollViewDelegate>
 @property (nonatomic) BCPriceChartView *priceChartView;
 @property (nonatomic) UIScrollView *scrollView;
 @property (nonatomic) NSMutableDictionary *addedCharts;
+@property (nonatomic) UIPageControl *pageControl;
+@property (nonatomic) BOOL isUsingPageControl;
+@property (nonatomic) CGFloat closeButtonOriginX;
+@property (nonatomic) UIButton *scrollViewCloseButton;
 @end
 
 @implementation BCPriceChartContainerViewController
@@ -26,12 +31,6 @@
     [super viewDidLoad];
     
     self.view.backgroundColor = [[UIColor blackColor] colorWithAlphaComponent:BUSY_VIEW_LABEL_ALPHA];
-    
-    CGFloat buttonWidth = 50;
-    UIButton *closeButton = [[UIButton alloc] initWithFrame:CGRectMake(self.view.frame.size.width - 8 - buttonWidth, 30, buttonWidth, buttonWidth)];
-    [closeButton setImage:[UIImage imageNamed:@"close"] forState:UIControlStateNormal];
-    [closeButton addTarget:self action:@selector(closeButtonTapped) forControlEvents:UIControlEventTouchUpInside];
-    [self.view addSubview:closeButton];
     
     self.addedCharts = [NSMutableDictionary new];
 }
@@ -44,20 +43,32 @@
 - (void)addPriceChartView:(BCPriceChartView *)priceChartView atIndex:(NSInteger)pageIndex
 {
     if (!self.scrollView) {
-        self.scrollView = [[UIScrollView alloc] initWithFrame:CGRectMake(0, 0, self.view.frame.size.width, priceChartView.frame.size.height)];
+        self.scrollView = [[UIScrollView alloc] initWithFrame:CGRectMake(0, 0, self.view.frame.size.width, self.view.frame.size.height)];
         self.scrollView.center = CGPointMake(self.scrollView.center.x, self.view.frame.size.height/2);
-        self.scrollView.contentSize = CGSizeMake(self.view.frame.size.width*3, self.scrollView.frame.size.height);
+        self.scrollView.contentSize = CGSizeMake(self.view.frame.size.width*3, priceChartView.frame.size.height);
         self.scrollView.pagingEnabled = YES;
-        self.scrollView.scrollEnabled = NO;
+        self.scrollView.scrollEnabled = YES;
+        self.scrollView.delegate = self;
         [self.scrollView setContentOffset:CGPointMake(pageIndex * self.scrollView.frame.size.width, 0) animated:NO];
         [self.view addSubview:self.scrollView];
         
-        UIPageControl *pageControl = [[UIPageControl alloc] initWithFrame:CGRectMake(0, self.scrollView.frame.origin.y + self.scrollView.frame.size.height + 16, 100, 30)];
+        priceChartView.center = CGPointMake(pageIndex * self.scrollView.frame.size.width + self.scrollView.frame.size.width/2, self.scrollView.frame.size.height/2);
+        
+        UIPageControl *pageControl = [[UIPageControl alloc] initWithFrame:CGRectMake(0, priceChartView.frame.origin.y + priceChartView.frame.size.height + 16, 100, 30)];
         pageControl.numberOfPages = 3;
         [pageControl setCurrentPage:pageIndex];
         [pageControl addTarget:self action:@selector(pageControlChanged:) forControlEvents:UIControlEventValueChanged];
         pageControl.center = CGPointMake(self.view.frame.size.width/2, pageControl.center.y);
+        self.pageControl = pageControl;
         [self.view addSubview:pageControl];
+        
+        CGFloat buttonWidth = 50;
+        UIButton *closeButton = [[UIButton alloc] initWithFrame:CGRectMake(pageIndex*self.view.frame.size.width + self.view.frame.size.width - 8 - buttonWidth, 30, buttonWidth, buttonWidth)];
+        self.closeButtonOriginX = closeButton.frame.origin.x;
+        [closeButton setImage:[UIImage imageNamed:@"close"] forState:UIControlStateNormal];
+        [closeButton addTarget:self action:@selector(closeButtonTapped) forControlEvents:UIControlEventTouchUpInside];
+        self.scrollViewCloseButton = closeButton;
+        [self.scrollView addSubview:closeButton];
     }
 
     priceChartView.center = CGPointMake(pageIndex * self.scrollView.frame.size.width + self.scrollView.frame.size.width/2, self.scrollView.frame.size.height/2);
@@ -65,6 +76,40 @@
     [self.scrollView addSubview:priceChartView];
     
     [self.addedCharts setObject:priceChartView forKey:[self dictionaryKeyForPage:pageIndex]];
+}
+
+- (void)scrollViewDidScroll:(UIScrollView *)scrollView
+{
+    if (!self.isUsingPageControl) {
+        CGFloat pageWidth = scrollView.frame.size.width;
+        float fractionalPage = scrollView.contentOffset.x / pageWidth;
+        NSInteger page = lround(fractionalPage);
+        if (self.pageControl.currentPage != page) {
+            self.pageControl.currentPage = page;
+            [self pageControlChanged:self.pageControl];
+        }
+    }
+
+    [self.scrollViewCloseButton changeXPosition:scrollView.contentOffset.x + self.closeButtonOriginX];
+}
+
+- (void)pageControlChanged:(UIPageControl *)pageControl
+{
+    self.isUsingPageControl = YES;
+    [self.scrollView setContentOffset:CGPointMake(pageControl.currentPage * self.scrollView.frame.size.width, 0) animated:YES];
+    
+    BCPriceChartView *priceChartView = [self.addedCharts objectForKey:[self dictionaryKeyForPage:pageControl.currentPage]];
+    if (!priceChartView) {
+        [self.delegate addPriceChartView:pageControl.currentPage];
+    } else {
+        self.priceChartView = priceChartView;
+        [self.delegate reloadPriceChartView:pageControl.currentPage];
+    }
+}
+
+- (void)scrollViewDidEndScrollingAnimation:(UIScrollView *)scrollView
+{
+    self.isUsingPageControl = NO;
 }
 
 - (void)clearChart
@@ -95,19 +140,6 @@
 - (void)updateTitleContainerWithChartDataEntry:(ChartDataEntry *)entry
 {
     [self.priceChartView updateTitleContainerWithChartDataEntry:entry];
-}
-
-- (void)pageControlChanged:(UIPageControl *)pageControl
-{
-    [self.scrollView setContentOffset:CGPointMake(pageControl.currentPage * self.scrollView.frame.size.width, 0) animated:YES];
-    
-    BCPriceChartView *priceChartView = [self.addedCharts objectForKey:[self dictionaryKeyForPage:pageControl.currentPage]];
-    if (!priceChartView) {
-        [self.delegate addPriceChartView:pageControl.currentPage];
-    } else {
-        self.priceChartView = priceChartView;
-        [self.delegate reloadPriceChartView:pageControl.currentPage];
-    }
 }
 
 - (void)updateEthExchangeRate:(NSDecimalNumber *)rate

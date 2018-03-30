322d049d787d057937aa35c2ee4c6a0f22e76be2
woudini <10579422+woudini@users.noreply.github.com>
2018-03-30 16:22:22 -0400

    fix(Transaction-Detail): set minimum scale factor

--- Blockchain/TransactionDetailFromCell.m
+++ Blockchain/TransactionDetailFromCell.m
@@ -44,6 +44,7 @@
     self.accessoryLabel.textColor = COLOR_TEXT_DARK_GRAY;
     self.accessoryLabel.textAlignment = NSTextAlignmentRight;
     self.accessoryLabel.adjustsFontSizeToFitWidth = YES;
+    self.accessoryLabel.minimumScaleFactor = 0.7;
     self.accessoryLabel.text = transactionModel.isContactTransaction && [transactionModel.txType isEqualToString:TX_TYPE_RECEIVED] ? transactionModel.contactName : transactionModel.fromString;
     
     CGFloat mainLabelHeight = self.mainLabel.frame.size.height;

bce8768e1698b79662095bfbf9d59a19bad488fd
Maurice Achtenhagen <maurice@blockchain.com>
2018-04-18 10:33:02 -0400

    feat(UserDefaults): add UserDefaults extension

---
+++ Blockchain/Extensions/UserDefaults.swift
@@ -0,0 +1,26 @@
+//
+//  UserDefaults.swift
+//  Blockchain
+//
+//  Created by Maurice A. on 4/13/18.
+//  Copyright © 2018 Blockchain Luxembourg S.A. All rights reserved.
+//
+
+import Foundation
+
+@objc
+extension UserDefaults {
+    enum DebugKeys: String {
+        case appReviewPromptTimer = "appReviewPromptTimer"
+        case enableCertificatePinning = "certificatePinning"
+        case securityReminderTimer = "securiterReminderTimer"
+        case simulateSurge = "simulateSurge"
+        case simulateZeroTicker = "zeroTicker"
+    }
+
+    enum Keys: String {
+        case assetType = "assetType"
+        case environment = "environment"
+        case swipeToReceiveEnabled = "swipeToReceive"
+    }
+}

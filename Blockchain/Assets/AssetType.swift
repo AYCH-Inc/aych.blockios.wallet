53dcd18cac79a3246f4f9c654e8093a25fbfcd9d
Maurice Achtenhagen <maurice@blockchain.com>
2018-04-18 10:27:40 -0400

    refactor(Asset Type): add asset type enum

---
+++ Blockchain/Assets/AssetType.swift
@@ -0,0 +1,14 @@
+//
+//  AssetType.swift
+//  Blockchain
+//
+//  Created by Maurice A. on 4/13/18.
+//  Copyright © 2018 Blockchain Luxembourg S.A. All rights reserved.
+//
+
+import Foundation
+
+/// The asset type is used to distinguish between different types of digital assets.
+public enum AssetType: Int {
+    case bitcoin, bitcoinCash, ethereum
+}

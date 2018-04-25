b7fdc2c3e2270f68cd2b3f11b6c1a273c5a3d187
Maurice Achtenhagen <maurice@blockchain.com>
2018-04-25 12:11:33 -0400

    chore(Bridging Header): do not expose openssl to Swift

--- Blockchain/Blockchain-Bridging-Header.h
+++ Blockchain/Blockchain-Bridging-Header.h
@@ -2,7 +2,6 @@
 //  Use this file to import your target's public headers that you would like to expose to Swift.
 //
 
-#import <openssl/x509.h>
 #import <UIKit/UIKit.h>
 #import "BCCreateWalletView.h"
 #import "BCFadeView.h"

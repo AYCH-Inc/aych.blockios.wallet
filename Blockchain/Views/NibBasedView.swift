58805a4dbab493f8868af25ae01535521e0fbda5
Alex McGregor <41585563+thisisalexmcgregor@users.noreply.github.com>
2018-08-06 13:57:02 -0500

    feat(KYC): New Address Screen & ValidationTextField (#375)
    * Progress commit - updating address UI to match new comps.
    
    * Progress commit - additional UI updates for address screen.
    
    * Progress commit - more UI updates for the address input screen
    
    * Progress commit for `ValidationTextField`
    
    * Handling keyboard presentation and offsetting the UIScrollView as the user proceeds through the fields.
    
    * Bug fixes for ValidationTextField and additional tweaks to the address UI.
    
    * No longer disabling the primaryButton.
    
    * Adding some comments
    
    * Removing files that are no longer in use.
    
    * Removing unnecessary print statement
    
    * Fixes an issue where the `cancel` button was not working correctly.
    
    * Setting up `UITextContentType` for `ValidationTextField`.

---
+++ Blockchain/Views/NibBasedView.swift
@@ -0,0 +1,45 @@
+//
+//  NibBasedView.swift
+//  Blockchain
+//
+//  Created by Alex McGregor on 8/1/18.
+//  Copyright © 2018 Blockchain Luxembourg S.A. All rights reserved.
+//
+
+import UIKit
+
+/// Subclass your UIView from NibLoadView
+/// to automatically load a xib with the same name
+/// as your class
+class NibBasedView: UIView {
+
+    @IBOutlet weak var view: UIView!
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        nibSetup()
+    }
+
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        nibSetup()
+    }
+
+    private func nibSetup() {
+        backgroundColor = .clear
+
+        view = loadViewFromNib()
+        view.frame = bounds
+        view.autoresizingMask = [.flexibleWidth, .flexibleHeight]
+        view.translatesAutoresizingMaskIntoConstraints = true
+
+        addSubview(view)
+    }
+
+    private func loadViewFromNib() -> UIView {
+        let bundle = Bundle(for: type(of: self))
+        let nib = UINib(nibName: String(describing: type(of: self)), bundle: bundle)
+        let nibView = nib.instantiate(withOwner: self, options: nil).first as! UIView
+        return nibView
+    }
+}

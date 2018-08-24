fb17f27ad06f3d3a4d604e9496fcb0c6dd5631a8
Maurice <maurice@achtenhagen.me>
2018-08-24 19:13:23 -0400

    feat(KYC): add Application Completed view controller (#430)
    * fix(KYC): include auth header in credential request
    
    * Creating OnfidoUser.
    
    * PR feedback.
    
    * feat(Nabu): IOS-1169 new authentication. (#428)
    
    * chore(KYC): add checkmark asset
    
    * feat(KYC): add Application Complete view controller
    
    * fix(KYC): update button title text
    
    * feat(KYC): show application complete screen after identity verification

---
+++ Blockchain/KYC/KYCApplicationCompleteController.swift
@@ -0,0 +1,38 @@
+//
+//  KYCApplicationCompleteController.swift
+//  Blockchain
+//
+//  Created by Maurice A. on 8/23/18.
+//  Copyright © 2018 Blockchain Luxembourg S.A. All rights reserved.
+//
+
+import UIKit
+
+final class KYCApplicationCompleteController: KYCBaseViewController, ProgressableView {
+
+    // MARK: - ProgressableView
+
+    @IBOutlet var progressView: UIProgressView!
+    var barColor: UIColor = .green
+    var startingValue: Float = 1.0
+
+    override class func make(with coordinator: KYCCoordinator) -> KYCApplicationCompleteController {
+        let controller = makeFromStoryboard()
+        controller.coordinator = coordinator
+        controller.pageType = .applicationComplete
+        return controller
+    }
+
+    // MARK: - View Lifecycle
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setupProgressView()
+    }
+
+    // MARK: - IBActions
+
+    @IBAction private func primaryButtonTapped(_ sender: Any) {
+        coordinator.presentAccountStatusView(for: .pending, in: self)
+    }
+}

7cfb2f2f8e3d2cd37b60373a2414be3a2c2c7f7f
woudini <10579422+woudini@users.noreply.github.com>
2018-10-02 18:31:35 -0400

    fix(Homebrew-Exchange): IOS-1382 remove order ID placeholder and display order ID in Exchange Locked screen (#562)
    * fix(TradeExecutionService): only POST to trades endpoint when sending order
    
    * clean(TradeExecutionService): remove unused variable
    
    * clean(TradeExecutionService): remove unused variables
    
    * fix(TradeExecutionService): use index of selected AssetAccount
    
    * clean(TradeExecutionService): reorganize methods
    
    * fix(Homebrew-Exchange): set order identifier in exchange locked screen
    
    * clean(TradeExecutionService): remove extra call to set isExecuting
    
    * fix(Homebrew-Exchange): use order identifier in Exchange Locked screen
    
    * fix(Homebrew-Exchange): move order ID to new cell on Exchange Locked screen
    
    * fix(Homebrew-Exchange): fix background color for order ID cell on Exchange Locked screen
    
    * refactor(Homebrew-Exchange): pass OrderTransaction struct instead of order identifier in success handlers

--- Blockchain/Homebrew/Exchange/API/TradeExecutionAPI.swift
+++ Blockchain/Homebrew/Exchange/API/TradeExecutionAPI.swift
@@ -24,7 +24,7 @@ protocol TradeExecutionAPI {
         with conversion: Conversion,
         from: AssetAccount,
         to: AssetAccount,
-        success: @escaping (() -> Void),
+        success: @escaping ((OrderTransaction) -> Void),
         error: @escaping ((String) -> Void)
     )
 
--- Blockchain/Homebrew/Exchange/Views/Headers/ExchangeLockedHeaderView.swift
+++ Blockchain/Homebrew/Exchange/Views/Headers/ExchangeLockedHeaderView.swift
@@ -8,26 +8,18 @@
 
 class ExchangeLockedHeaderView: UICollectionReusableView {
     
-    // MARK: Public
+    // MARK: - Public
     
     static let identifier: String = String(describing: ExchangeLockedHeaderView.self)
     var closeTapped: (() -> Void)?
     
-    // MARK Private Static Properties
+    // MARK: - Private Static Properties
     
-    static fileprivate let verticalPadding: CGFloat = 82.0
+    static fileprivate let verticalPadding: CGFloat = 52.0
     
-    // MARK: Private IBOutlets
+    // MARK: - Private IBOutlets
     
     @IBOutlet fileprivate var title: UILabel!
-    @IBOutlet fileprivate var orderID: UILabel!
-    
-    var orderIdentifier: String? {
-        didSet {
-            guard let identifier = orderIdentifier else { return }
-            orderID.text = LocalizationConstants.Exchange.orderID + " " + identifier
-        }
-    }
     
     override func awakeFromNib() {
         super.awakeFromNib()
@@ -35,7 +27,7 @@ class ExchangeLockedHeaderView: UICollectionReusableView {
         title.text = LocalizationConstants.Exchange.exchangeLocked
     }
     
-    // MARK: Actions
+    // MARK: - Actions
     
     @IBAction func closeButtonTapped(_ sender: UIButton) {
         closeTapped?()
@@ -44,8 +36,6 @@ class ExchangeLockedHeaderView: UICollectionReusableView {
     static func estimatedHeight() -> CGFloat {
         
         guard let titleFont = UIFont(name: Constants.FontNames.montserratRegular, size: 20) else { return 0.0 }
-        guard let orderFont = UIFont(name: Constants.FontNames.montserratRegular, size: 16) else { return 0.0 }
-        
         
         let attributedTitle = NSAttributedString(
             string: LocalizationConstants.Exchange.exchangeLocked,
@@ -53,12 +43,6 @@ class ExchangeLockedHeaderView: UICollectionReusableView {
                 NSAttributedStringKey.font: titleFont
             ]
         )
-        let attributedOrder = NSAttributedString(
-            string: LocalizationConstants.Exchange.exchangeLocked,
-            attributes: [
-                NSAttributedStringKey.font: orderFont
-            ]
-        )
-        return verticalPadding + attributedTitle.height + attributedOrder.height
+        return verticalPadding + attributedTitle.height
     }
 }

4069315a2516c838543b67e8b5439cf8778d8d38
Maurice Achtenhagen <maurice@blockchain.com>
2018-10-24 13:26:30 -0400

    refactor(Dashboard): Swift implementation of BCPricePreviewView

---
+++ Blockchain/Dashboard/Charts/PricePreviewView/PricePreviewView.swift
@@ -0,0 +1,59 @@
+//
+//  PricePreviewView.swift
+//  Blockchain
+//
+//  Created by Maurice A. on 10/23/18.
+//  Copyright © 2018 Blockchain Luxembourg S.A. All rights reserved.
+//
+
+import UIKit
+
+class PricePreviewView: UIView {
+
+    // MARK: - Properties
+
+    var seeChartsButtonHandler: (() -> Void)?
+
+    var currencyTitle: String {
+        get {
+            return currencyTitleLabel.text ?? ""
+        }
+        set {
+            currencyTitleLabel.text = newValue
+        }
+    }
+
+    var price: String {
+        get {
+            return priceLabel.text ?? ""
+        }
+        set {
+            priceLabel.text = newValue
+        }
+    }
+
+    // MARK: - IBOutlets
+
+    @IBOutlet private var currencyTitleLabel: UILabel!
+    @IBOutlet private var priceLabel: UILabel!
+
+    // MARK: - IBActions
+
+    @IBAction private func seeCharts(_ sender: UIButton) {
+        seeChartsButtonHandler?()
+    }
+
+    // MARK: - Initialization
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+    }
+
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+    }
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+    }
+}

b1474fe57d65d60d3d9cb64cc4d97f7265640639
Maurice Achtenhagen <maurice@blockchain.com>
2018-11-01 11:15:34 -0400

    fix(Dashboard): show XLM transactions view when balance legend key tapped

--- Blockchain/Models/TabControllerManager.m
+++ Blockchain/Models/TabControllerManager.m
@@ -15,6 +15,7 @@
 
 @property (strong, nonatomic) DashboardController *dashboardViewController;
 @property (strong, nonatomic) SendLumensViewController *sendLumensViewController;
+@property (strong, nonatomic) TransactionsXlmViewController *transactionsStellarViewController;
 
 @end
 @implementation TabControllerManager
@@ -547,8 +548,10 @@
 
         [_tabViewController setActiveViewController:_transactionsBitcoinCashViewController animated:animated index:tabIndex];
     } else if (self.assetType == LegacyAssetTypeStellar) {
-        TransactionsXlmViewController *viewController = [TransactionsXlmViewController makeWith:XLMServiceProvider.sharedInstance];
-        [_tabViewController setActiveViewController:viewController animated:animated index:tabIndex];
+        if (_transactionsStellarViewController) {
+            _transactionsStellarViewController = [TransactionsXlmViewController makeWith:XLMServiceProvider.sharedInstance];
+        }
+        [_tabViewController setActiveViewController:_transactionsStellarViewController animated:animated index:tabIndex];
     }
 }
 
@@ -752,6 +755,14 @@
     [self changeAssetSelectorAsset:LegacyAssetTypeBitcoinCash];
     [self showTransactionsAnimated:YES];
     [_transactionsBitcoinCashViewController reload];
+}
+
+-(void)showTransactionsStellar
+{
+    [self changeAssetSelectorAsset:LegacyAssetTypeStellar];
+    [self showTransactionsAnimated:YES];
+    // TODO: reload transactions
+    // [_transactionsStellarViewController reload];
 }
 
 - (void)changeAssetSelectorAsset:(LegacyAssetType)assetType

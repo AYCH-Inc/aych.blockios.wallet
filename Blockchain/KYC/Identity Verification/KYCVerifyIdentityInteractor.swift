ef8ca3a0a9e86929db8a1ad92bb80872c7f60d8f
woudini <10579422+woudini@users.noreply.github.com>
2019-01-29 18:17:07 -0500

    refactor(KYC): IOS-1851 add KYCResubmitIdentityController and refactor KYCVerifyIdentityController (#288)
    * feat(AnnouncementCards): configure view model for documents needed
    
    * refactor(CardsViewController): rename file
    
    * feat(KYC): add router for document resubmission
    
    * feat(NabuUser): add property for document resubmission tag
    
    * feat(AnnouncementCards): show upload documents card if user is tagged for document resubmission
    
    * refactor(AnnouncementCards): combine localized strings for document resubmission description
    
    * refactor(AnnouncementCards): rename CardsViewController extension
    
    * fix(KYC): fix tests
    
    * fix(KYC): fix tests
    
    * feat(DeepLink): configure handling for current deeplink URL
    
    * feat(NabuUser): add DocumentResubmission struct
    
    * fix(AnnouncementCards): show upload documents card if resubmission property is not nil
    
    * refactor(KYCVerifyIdentityController): add VeriffController protocol
    
    * feat(KYC): add ResubmitIdentity controller and storyboard
    
    * feat(KYCResubmitIdentity): add navigation item and connect button container outlet
    
    * feat(KYC): localize KYCResubmitIdentity storyboard file
    
    * fix(KYC): fix custom module target for KYCResubmitIdentity storyboard
    
    * fix(KYC): change button title
    
    * refactor(KYC): move RxSwift components to KYCVerifyIdentityInteractor
    
    * fix(KYC): correctly set delegate
    
    * fix(KYC): silence warning about unused variable
    
    * refactor(DeepLinking): store routers in DeepLinkRouter
    
    * fix(DeepLinkRoute): update key and value params
    
    * fix(KYCResubmitIdentity): remove view delegate method implementation and correctly set delegate
    
    * refactor(KYC): remove RxSwift from identity presenter and separate document delegate method from KYCResubmitIdentityController
    
    * feat(KYCPageType): add page type for .resubmitIdentity
    
    * refactor(KYC): force unwrap KYC identity verification presenter

--- Blockchain/KYC/Identity Verification/KYCVerifyIdentityInteractor.swift
+++ Blockchain/KYC/Identity Verification/KYCVerifyIdentityInteractor.swift
@@ -8,16 +8,43 @@
 
 import RxSwift
 
+protocol KYCVerifyIdentityInput: class {
+    func submitVerification(
+        onCompleted: @escaping (() -> Void),
+        onError: @escaping ((Error) -> Void)
+    )
+    func createCredentials(
+        onSuccess: @escaping ((VeriffCredentials) -> Void),
+        onError: @escaping ((Error) -> Void)
+    )
+    func supportedDocumentTypes(
+        countryCode: String,
+        onSuccess: @escaping (([KYCDocumentType]) -> Void),
+        onError: @escaping ((Error) -> Void)
+    )
+}
+
 class KYCVerifyIdentityInteractor {
     private let authentication: NabuAuthenticationService
 
     private var cache = [String: [KYCDocumentType]]()
 
+    private let veriffService = VeriffService()
+
+    private var veriffCredentials: VeriffCredentials?
+
+    private var disposable: Disposable?
+
     init(authentication: NabuAuthenticationService = NabuAuthenticationService.shared) {
         self.authentication = authentication
     }
 
-    func supportedDocumentTypes(_ countryCode: String) -> Single<[KYCDocumentType]> {
+    deinit {
+        disposable?.dispose()
+        disposable = nil
+    }
+
+    private func supportedDocumentTypes(_ countryCode: String) -> Single<[KYCDocumentType]> {
         // Check cache
         if let types = cache[countryCode] {
             return Single.just(types)
@@ -39,5 +66,38 @@ class KYCVerifyIdentityInteractor {
         .do(onSuccess: { [weak self] types in
             self?.cache[countryCode] = types
         })
+    }
+}
+
+extension KYCVerifyIdentityInteractor: KYCVerifyIdentityInput {
+    func submitVerification(
+        onCompleted: @escaping (() -> Void),
+        onError: @escaping ((Error) -> Void)
+    ) {
+        guard let credentials = veriffCredentials else { return }
+        disposable = veriffService.submitVerification(applicantId: credentials.applicantId)
+            .do(onDispose: { LoadingViewPresenter.shared.hideBusyView() })
+            .subscribe(
+                onCompleted: onCompleted,
+                onError: onError
+            )
+    }
+
+    func createCredentials(onSuccess: @escaping ((VeriffCredentials) -> Void), onError: @escaping ((Error) -> Void)) {
+        disposable = veriffService.createCredentials()
+            .subscribeOn(MainScheduler.asyncInstance)
+            .observeOn(MainScheduler.instance)
+            .subscribe(onSuccess: { [weak self] credentials in
+                guard let this = self else { return }
+                this.veriffCredentials = credentials
+                onSuccess(credentials)
+            }, onError: onError)
+    }
+
+    func supportedDocumentTypes(countryCode: String, onSuccess: @escaping (([KYCDocumentType]) -> Void), onError: @escaping ((Error) -> Void)) {
+        disposable = supportedDocumentTypes(countryCode)
+            .subscribeOn(MainScheduler.asyncInstance)
+            .observeOn(MainScheduler.instance)
+            .subscribe(onSuccess: onSuccess, onError: onError)
     }
 }
--- Blockchain/KYC/Identity Verification/KYCVerifyIdentityPresenter.swift
+++ Blockchain/KYC/Identity Verification/KYCVerifyIdentityPresenter.swift
@@ -6,63 +6,84 @@
 //  Copyright © 2018 Blockchain Luxembourg S.A. All rights reserved.
 //
 
-import RxSwift
+import PlatformUIKit
 
 protocol KYCVerifyIdentityView: class {
-    func showLoadingIndicator()
+    func showCameraPermissionsDenied()
 
-    func hideLoadingIndicator()
+    func promptToAcceptCameraPermissions()
+
+    func sendToVeriff()
 
     func showDocumentTypes(_ types: [KYCDocumentType])
+}
 
-    func showErrorMessage(_ message: String)
-    
-    func showCameraPermissionsDenied()
-    
-    func promptToAcceptCameraPermissions()
-    
-    func sendToVeriff()
+protocol KYCVerifyIdentityDelegate: class {
+    func submitVerification(
+        onCompleted: @escaping (() -> Void),
+        onError: @escaping ((Error) -> Void)
+    )
+    func createCredentials(
+        onSuccess: @escaping ((VeriffCredentials) -> Void),
+        onError: @escaping ((Error) -> Void)
+    )
 }
 
 class KYCVerifyIdentityPresenter {
     private let interactor: KYCVerifyIdentityInteractor
-    private weak var view: KYCVerifyIdentityView?
-    private var disposable: Disposable?
+    private weak var loadingView: LoadingView?
 
-    deinit {
-        disposable?.dispose()
-        disposable = nil
-    }
+    // TODO: Separate and use in a different presenter specifically made
+    // for the KYCVerifyIdentityViewController.
+    weak var identityView: KYCVerifyIdentityView?
 
-    init(interactor: KYCVerifyIdentityInteractor, view: KYCVerifyIdentityView) {
+    private lazy var permissionsRequestor: PermissionsRequestor = {
+        return PermissionsRequestor()
+    }()
+
+    init(
+        interactor: KYCVerifyIdentityInteractor,
+        loadingView: LoadingView
+    ) {
         self.interactor = interactor
-        self.view = view
+        self.loadingView = loadingView
     }
 
     func presentDocumentTypeOptions(_ countryCode: String) {
-        view?.showLoadingIndicator()
-        disposable = interactor.supportedDocumentTypes(countryCode)
-            .subscribeOn(MainScheduler.asyncInstance)
-            .observeOn(MainScheduler.instance)
-            .subscribe(onSuccess: { [weak self] documentTypes in
-                self?.view?.hideLoadingIndicator()
-                self?.view?.showDocumentTypes(documentTypes)
-            }, onError: { [weak self] error in
+        loadingView?.showLoadingIndicator()
+        interactor.supportedDocumentTypes(
+            countryCode: countryCode,
+            onSuccess: { [weak self] documentTypes in
+                self?.loadingView?.hideLoadingIndicator()
+                self?.identityView?.showDocumentTypes(documentTypes)
+            },
+            onError: { [weak self] error in
                 Logger.shared.error("Error: \(error.localizedDescription)")
-                self?.view?.hideLoadingIndicator()
-                self?.view?.showErrorMessage(LocalizationConstants.Errors.genericError)
-            })
+                self?.loadingView?.hideLoadingIndicator()
+                self?.loadingView?.showErrorMessage(LocalizationConstants.Errors.genericError)
+            }
+        )
+    }
+}
+
+extension KYCVerifyIdentityPresenter: KYCVerifyIdentityDelegate {
+    func submitVerification(onCompleted: @escaping (() -> Void), onError: @escaping ((Error) -> Void)) {
+        interactor.submitVerification(onCompleted: onCompleted, onError: onError)
+    }
+
+    func createCredentials(onSuccess: @escaping ((VeriffCredentials) -> Void), onError: @escaping ((Error) -> Void)) {
+        interactor.createCredentials(onSuccess: onSuccess, onError: onError)
     }
     
     func didTapNext() {
         if PermissionsRequestor.shouldDisplayCameraPermissionsRequest() {
-            view?.promptToAcceptCameraPermissions()
+            identityView?.promptToAcceptCameraPermissions()
             return
         }
         if PermissionsRequestor.cameraRefused() == false {
-            view?.sendToVeriff()
+            identityView?.sendToVeriff()
         } else {
-            view?.showCameraPermissionsDenied()
+            identityView?.showCameraPermissionsDenied()
         }
     }
     
@@ -71,14 +92,10 @@ class KYCVerifyIdentityPresenter {
             guard let this = self else { return }
             switch PermissionsRequestor.cameraEnabled() {
             case true:
-                this.view?.sendToVeriff()
+                this.identityView?.sendToVeriff()
             case false:
-                this.view?.showCameraPermissionsDenied()
+                this.identityView?.showCameraPermissionsDenied()
             }
         }
     }
-    
-    private lazy var permissionsRequestor: PermissionsRequestor = {
-       return PermissionsRequestor()
-    }()
 }

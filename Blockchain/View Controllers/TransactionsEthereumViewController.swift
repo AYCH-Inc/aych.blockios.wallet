69e15fac11fdbe925c1daad06ae4647cad87be7b
Jack Pooley <47525610+jackpooleybc@users.noreply.github.com>
2019-03-25 15:10:03 +0000

    IOS-2084 Implement correct tableView delegate method (#391)

--- Blockchain/View Controllers/TransactionsEthereumViewController.swift
+++ Blockchain/View Controllers/TransactionsEthereumViewController.swift
@@ -177,7 +177,7 @@ extension TransactionsEthereumViewController: UITableViewDataSource {
 }
 
 extension TransactionsEthereumViewController: UITableViewDelegate {
-    func tableView(_ tableView: UITableView, didDeselectRowAt indexPath: IndexPath) {
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         tableView.deselectRow(at: indexPath, animated: true)
         if let cell = tableView.cellForRow(at: indexPath) as? TransactionEtherTableViewCell {
            cell.transactionClicked()

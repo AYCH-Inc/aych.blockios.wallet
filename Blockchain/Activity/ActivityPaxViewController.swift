db7426f71c9e31b450593cc4774093cc1004ece7
Jack Pooley <47525610+jackpooleybc@users.noreply.github.com>
2019-04-24 20:05:09 +0100

    IOS-2056 (#436)

---
+++ Blockchain/Activity/ActivityPaxViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ActivityPaxViewController.swift
+//  Blockchain
+//
+//  Created by Jack on 23/04/2019.
+//  Copyright © 2019 Blockchain Luxembourg S.A. All rights reserved.
+//
+
+import UIKit
+
+class ActivityPaxViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // TODO as part of `IOS-2124`
+        
+        view.backgroundColor = UIColor.red
+    }
+}

c7455398ab9334a43166a831d3caeab75748b1f7
Chris <38220701+bchrisarriola@users.noreply.github.com>
2019-08-15 11:08:49 -0700

    fix(Bitpay): IOS-2466 hide the pit button if the user scans a bitpay QR code. (#672)

--- Blockchain/View Controllers/SendBitcoinViewController.m
+++ Blockchain/View Controllers/SendBitcoinViewController.m
@@ -1059,6 +1059,7 @@ BOOL displayingLocalSymbolSend;
     UIAlertController *expiredAlert = [UIAlertController alertControllerWithTitle:BC_STRING_BITPAY_INVOICE_EXPIRED_TITLE message:BC_STRING_BITPAY_INVOICE_EXPIRED_MESSAGE preferredStyle:UIAlertControllerStyleAlert];
     [expiredAlert addAction:[UIAlertAction actionWithTitle:BC_STRING_OK style:UIAlertActionStyleCancel handler:^(UIAlertAction * _Nonnull action) {
         [self.navigationController popViewControllerAnimated:YES];
+        [self reload];
     }]];
     [[UIApplication sharedApplication].keyWindow.rootViewController presentViewController:expiredAlert animated:YES completion:nil];
     [[ModalPresenter sharedInstance] closeModalWithTransition:kCATransitionFade];
@@ -2201,6 +2202,8 @@ BOOL displayingLocalSymbolSend;
                             self.bitpayTimeRemainingText.hidden = NO;
                             self.lineBelowBitpayLabel.hidden = NO;
 
+                            // Hide Pit label
+                            self.pitAddressButton.hidden = YES;
 
                             //Set time remaining string
                             self.bitpayExpiration = paymentReq.expires;
